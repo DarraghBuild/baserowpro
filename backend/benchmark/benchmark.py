d0e737a2bfac82df767ebc2e227f00e35306bfbb
peter_baserow <peter@baserow.io>
2023-04-03 12:33:07 +0100

    Adding a basic benchmarking script so that we can easily test the least performant requests in tables with many fields.

---
+++ backend/benchmark/benchmark.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+from pathlib import Path
+
+import psycopg2
+import requests
+from django.conf import settings
+
+DEFAULT_DB = settings.DATABASES["default"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--initdb",
+    action="store_true",
+    help="Should the current database be replaced with our benchmarking database?",
+)
+parser.add_argument(
+    "--scenario",
+    action="store",
+    help="Execute an HTTP request which is experiencing performance issues.",
+)
+
+
+class BackendBenchmark:
+    @classmethod
+    def initdb(cls):
+        connection = psycopg2.connect(
+            database="postgres",
+            user=DEFAULT_DB["USER"],
+            password=DEFAULT_DB["PASSWORD"],
+            host=DEFAULT_DB["HOST"],
+        )
+        connection.autocommit = True
+        cursor = connection.cursor()
+        confirm = input(
+            "Calling --initdb will drop your current database, are you "
+            "sure you want to do this? y/n\n"
+        ).lower()
+        if confirm != "y":
+            return
+
+        # Double-check the grid view many field SQL exists.
+        if not Path("samples/grid-view-many-fields.sql").is_file():
+            raise EnvironmentError("Please download the sample SQL.")
+
+        try:
+            cursor.execute(f"DROP DATABASE IF EXISTS {DEFAULT_DB['NAME']}")
+            cursor.execute(f"CREATE DATABASE {DEFAULT_DB['NAME']}")
+        except psycopg2.errors.ObjectInUse:
+            raise EnvironmentError("The database cannot be re-created if it is use.")
+
+        subprocess.run(
+            f"psql -U {DEFAULT_DB['USER']} -h {DEFAULT_DB['HOST']} "
+            f"{DEFAULT_DB['NAME'] } < samples/grid-view-many-fields.sql",
+            shell=True,
+            env={"PGPASSWORD": DEFAULT_DB["PASSWORD"]},
+        )
+
+    @classmethod
+    def get_jwt(cls):
+        response = requests.post(
+            f"{settings.PUBLIC_BACKEND_URL}/api/user/token-auth/",
+            {"email": "dev@baserow.io", "password": "testpassword"},
+        )
+        response.raise_for_status()
+        return response.json()["access_token"]
+
+    @classmethod
+    def scenario(cls, scenario: str):
+        scenarios = {
+            "rows": "/api/database/views/grid/1910/?limit=120&offset=0"
+            "&include=row_metadata",
+            "search": "/api/database/views/grid/1910/?limit=120&offset=0"
+            "&include=row_metadata&search=sarah",
+            "aggregations": "/api/database/views/grid/1910/aggregations",
+        }
+        if scenario not in scenarios:
+            raise ValueError(
+                f"Invalid test scenario, please use: {', '.join(scenarios.keys())}"
+            )
+        url = settings.PUBLIC_BACKEND_URL + scenarios[scenario]
+        requests.get(url, headers={"Authorization": f"JWT {cls.get_jwt()}"})
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.initdb:
+        BackendBenchmark.initdb()
+    if args.scenario:
+        BackendBenchmark.scenario(args.scenario)
---
+++ backend/benchmark/samples/README.md
@@ -0,0 +1,4 @@
+# Benchmarking SQL
+
+This directory contains the SQL which we'll initially use to setup our benchmarking 
+environments.
