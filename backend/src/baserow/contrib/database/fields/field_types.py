acf373ae2d88f9f0129b4d153e58cba4e4d5ced6
Mansur Kutybayev <mansur@blaze.today>
2023-09-13 13:18:24 +0600

    Ignore multipleselect field values that are missing in select options

--- backend/src/baserow/contrib/database/fields/field_types.py
+++ backend/src/baserow/contrib/database/fields/field_types.py
@@ -3173,7 +3173,9 @@ class MultipleSelectFieldType(SelectOptionBaseFieldType):
         self, row, field_name, value, id_mapping, cache, files_zip, storage
     ):
         mapped_values = [
-            id_mapping["database_field_select_options"][item] for item in value
+            id_mapping["database_field_select_options"][item]
+            for item in value
+            if item in id_mapping["database_field_select_options"]
         ]
         getattr(row, field_name).set(mapped_values)
 
