0a5b071262e8e7eb0149c1ade2a8fa8320148d11
Nigel Gott <nigel@baserow.io>
2023-09-19 14:06:03 +0100

    Add basic file field support to the formula language.

---
+++ web-frontend/modules/database/components/card/RowCardFieldSingleFile.vue
@@ -0,0 +1,32 @@
+<template functional>
+  <div class="card-file__list-wrapper">
+    <ul class="card-file__list">
+      <li class="card-file__item">
+        <img
+          v-if="props.value?.is_image"
+          class="card-file__image"
+          :src="props.value?.thumbnails?.tiny?.url"
+        />
+        <i
+          v-else
+          class="fas card-file__icon"
+          :class="'fa-' + $options.methods.getIconClass(props.value.mime_type)"
+        ></i>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mimetype2fa } from '@baserow/modules/core/utils/fontawesome'
+
+export default {
+  height: 22,
+  name: 'RowCardFieldSingleFile',
+  methods: {
+    getIconClass(mimeType) {
+      return mimetype2fa(mimeType)
+    },
+  },
+}
+</script>
---
+++ web-frontend/modules/database/components/view/grid/fields/FunctionalGridViewSingleFile.vue
@@ -0,0 +1,31 @@
+<template functional>
+  <div class="grid-view__cell grid-field-file__cell">
+    <div v-if="props.value" class="grid-field-file__item">
+      <a class="grid-field-file__link">
+        <img
+          v-if="props.value.is_image"
+          class="grid-field-file__image"
+          :src="props.value?.thumbnails?.tiny?.url"
+        />
+        <i
+          v-else
+          class="fas grid-field-file__icon"
+          :class="'fa-' + $options.methods.getIconClass(props.value.mime_type)"
+        ></i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mimetype2fa } from '@baserow/modules/core/utils/fontawesome'
+
+export default {
+  name: 'FunctionalGridViewSingleFile',
+  methods: {
+    getIconClass(mimeType) {
+      return mimetype2fa(mimeType)
+    },
+  },
+}
+</script>
