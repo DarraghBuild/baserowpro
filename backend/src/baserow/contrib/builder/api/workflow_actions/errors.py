1b0955a1a075071f898fc54971d7630cf8a5a509
alex1431999 <alex@baserow.io>
2023-10-04 15:40:05 +0200

    fix error msg

--- backend/src/baserow/contrib/builder/api/workflow_actions/errors.py
+++ backend/src/baserow/contrib/builder/api/workflow_actions/errors.py
@@ -3,5 +3,5 @@ from rest_framework.status import HTTP_404_NOT_FOUND
 ERROR_WORKFLOW_ACTION_DOES_NOT_EXIST = (
     "ERROR_WORKFLOW_ACTION_DOES_NOT_EXIST",
     HTTP_404_NOT_FOUND,
-    "The requested element does not exist.",
+    "The requested workflow action does not exist.",
 )
