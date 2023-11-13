bec5161fd58dbc891a84ea411404a2226c51ac4d
Jonathan Adeline <jonathan@baserow.io>
2023-11-13 17:29:19 +0400

    Add chips component

---
+++ web-frontend/modules/core/components/Chips.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="chips" :class="{ 'chips--active': active }" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Chips',
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
