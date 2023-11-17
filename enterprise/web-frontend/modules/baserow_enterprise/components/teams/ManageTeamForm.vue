b2a8f1ba61e014a0064c1843c5b2e0a846f183ac
Jonathan Adeline <jonathan@baserow.io>
2023-11-17 19:05:15 +0400

    Use avatar component

--- enterprise/web-frontend/modules/baserow_enterprise/components/teams/ManageTeamForm.vue
+++ enterprise/web-frontend/modules/baserow_enterprise/components/teams/ManageTeamForm.vue
@@ -81,9 +81,12 @@
           :attributes="[]"
         >
           <template #left-side="{ item }">
-            <div class="select-members-list__user-initials margin-left-1">
-              {{ item.name | nameAbbreviation }}
-            </div>
+            <Avatar
+              class="margin-left-1"
+              rounded
+              size="medium"
+              :initials="item.name | nameAbbreviation"
+            ></Avatar>
             <span class="margin-left-1">
               {{ item.name }}
             </span>
