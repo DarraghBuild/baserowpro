2b17610331a8ecdf060b9d6183d60234c4861793
Jonathan Adeline <jonathan@baserow.io>
2023-11-21 16:11:11 +0400

    Add segmentControl component

---
+++ web-frontend/modules/core/components/SegmentControl.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="segment-control">
+    <button
+      v-for="(segment, index) in segments"
+      :key="index"
+      :class="{ 'segment-control__button--active': index === activeIndex }"
+      class="segment-control__button"
+      @click="setActiveIndex(index)"
+    >
+      {{ segment }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SegmentControl',
+  props: {
+    /**
+     * The segments to display.
+     */
+    segments: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+    /**
+     * The index of the active segment.
+     */
+    initialActiveIndex: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      activeIndex: this.initialActiveIndex,
+    }
+  },
+  methods: {
+    setActiveIndex(index) {
+      this.activeIndex = index
+      this.$emit('update:activeIndex', index)
+    },
+  },
+}
+</script>
