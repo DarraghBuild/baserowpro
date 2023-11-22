d8ea9c24dd3f0484bd21de7ac47a67baf466fe21
Jonathan Adeline <jonathan@baserow.io>
2023-11-22 17:03:06 +0400

    button components

---
+++ web-frontend/modules/core/components/ButtonIcon.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    class="button-icon"
+    :class="classes"
+    :disabled="disabled"
+    v-on="$listeners"
+  >
+    <i v-if="!loading" class="button-icon__icon" :class="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+     * The size of the button.
+     */
+    icon: {
+      required: true,
+      type: String,
+      default: '',
+    },
+    /**
+     * If true a loading icon will be shown.
+     */
+    loading: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * If true the button will be disabled.
+     */
+    disabled: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * If true the button will be disabled.
+     */
+    active: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classes() {
+      const classObj = {
+        'button-icon--loading': this.loading,
+        disabled: this.disabled,
+        active: this.active,
+      }
+      return classObj
+    },
+  },
+}
+</script>
---
+++ web-frontend/modules/core/components/ButtonText.vue
@@ -0,0 +1,138 @@
+<template>
+  <component
+    :is="tag === 'a' || href ? 'a' : 'button'"
+    class="button-text"
+    :class="classes"
+    :disabled="disabled"
+    :active="active"
+    :rel="rel"
+    v-bind.prop="customBind"
+    v-on="$listeners"
+  >
+    <i
+      v-if="icon !== '' && !loading"
+      class="button-text__icon"
+      :class="`iconoir-${icon}`"
+    />
+
+    <i v-if="loading" class="button-text__loading"></i>
+    <span><slot /></span>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+     * The tag to use for the root element.
+     */
+    tag: {
+      required: false,
+      type: String,
+      default: 'button',
+      validator: function (value) {
+        return ['a', 'button'].includes(value)
+      },
+    },
+    /**
+     * The size of the button.
+     */
+    size: {
+      required: false,
+      type: String,
+      default: 'regular',
+      validator: function (value) {
+        return ['regular', 'small'].includes(value)
+      },
+    },
+    /**
+     * The type of the button.
+     */
+    type: {
+      required: false,
+      type: String,
+      default: 'primary',
+      validator: function (value) {
+        return ['primary', 'secondary'].includes(value)
+      },
+    },
+    /**
+     * The icon of the button. Must be a valid iconoir or baserow icon class name.
+     */
+    icon: {
+      required: false,
+      type: String,
+      default: '',
+    },
+    /**
+     * If true a loading icon will be shown.
+     */
+    loading: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * If true the button will be disabled.
+     */
+    disabled: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * If true the button will be active.
+     */
+    active: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * The href attribute of the button.
+     */
+    href: {
+      required: false,
+      type: String,
+      default: '',
+    },
+    /**
+     * The rel attribute of the button.
+     */
+    rel: {
+      required: false,
+      type: String,
+      default: '',
+    },
+    /**
+     * The target attribute of the button.
+     */
+    target: {
+      required: false,
+      type: String,
+      validator: function (value) {
+        return ['_blank', '_self'].includes(value)
+      },
+      default: '_self',
+    },
+  },
+  computed: {
+    classes() {
+      const classObj = {
+        [`button-text--${this.size}`]: this.size !== 'regular',
+        [`button-text--${this.type}`]: this.type !== 'primary',
+        'button-text--loading': this.loading,
+        disabled: this.disabled,
+        active: this.active,
+      }
+      return classObj
+    },
+    customBind() {
+      const attr = {}
+      if (this.href) attr.href = this.href
+      if (this.target) attr.target = `${this.target}`
+      return attr
+    },
+  },
+}
+</script>
