f04322e72a80eac118b95d22e2515b48e7caec17
Petr Stribny <petr@stribny.name>
2023-11-28 16:17:45 +0100

    wip

--- backend/tests/baserow/api/test_api_decorators.py
+++ backend/tests/baserow/api/test_api_decorators.py
@@ -4,7 +4,8 @@ from unittest.mock import MagicMock
 from django.db import OperationalError
 
 import pytest
-import pytz
+import zoneinfo
+from datetime import timezone
 from rest_framework import serializers, status
 from rest_framework.exceptions import APIException
 from rest_framework.parsers import JSONParser
@@ -552,7 +553,7 @@ def test_accept_timezone():
 
     @accept_timezone()
     def test_1(self, request, now):
-        assert now.tzinfo == pytz.utc
+        assert now.tzinfo == timezone.utc
 
     test_1(None, request)
 
@@ -565,6 +566,6 @@ def test_accept_timezone():
 
     @accept_timezone()
     def test_1(self, request, now):
-        assert now.tzinfo == pytz.timezone("Etc/GMT+2")
+        assert now.tzinfo == zoneinfo.ZoneInfo("Etc/GMT+2")
 
     test_1(None, request)
