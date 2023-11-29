ced3ff9c0a598b5a8531bd0e67c98db05025b85f
Jonathan Adeline <jonathan@baserow.io>
2023-11-29 17:01:08 +0400

    address feedback

--- web-frontend/modules/core/components/Chips.vue
+++ web-frontend/modules/core/components/Chips.vue
@@ -1,5 +1,10 @@
 <template>
-  <button class="chips" :class="{ 'chips--active': active }" v-on="$listeners">
+  <button
+    class="chips"
+    :class="{ 'chips--active': active, 'chips--disabled': disabled }"
+    v-on="$listeners"
+  >
+    <i v-if="icon" class="chips__icon" :class="icon" />
     <slot />
   </button>
 </template>
@@ -9,8 +14,19 @@ export default {
   name: 'Chips',
   props: {
     active: {
+      required: false,
       type: Boolean,
       default: false,
+    },
+    disabled: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      required: false,
+      type: String,
+      default: '',
     },
   },
 }
