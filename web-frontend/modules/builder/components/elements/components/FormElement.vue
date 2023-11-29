f7a371cde46e83e9fc238b64140676eda21fd83a
alex1431999 <alex@baserow.io>
2023-11-29 14:23:30 +0100

    Render children in form container

--- web-frontend/modules/builder/components/elements/components/FormElement.vue
+++ web-frontend/modules/builder/components/elements/components/FormElement.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="mode === 'editing'">
+  <div v-if="mode === 'editing' && children.length === 0">
     <AddElementZone @add-element="showAddElementModal"></AddElementZone>
     <AddElementModal
       ref="addElementModal"
@@ -7,16 +7,34 @@
       :element-types-allowed="elementType.childElementTypes"
     ></AddElementModal>
   </div>
+  <div v-else>
+    <template v-for="(child, index) in children">
+      <ElementPreview
+        v-if="mode === 'editing'"
+        :key="child.id"
+        class="element"
+        :element="child"
+        :active="child.id === elementSelectedId"
+        :index="index"
+        :placements="[PLACEMENTS.BEFORE, PLACEMENTS.AFTER]"
+        :placements-disabled="getPlacementsDisabledVertical(index)"
+        @move="moveVertical(child, index, $event)"
+      />
+      <PageElement v-else :key="child.id" :element="child" :mode="mode" />
+    </template>
+  </div>
 </template>
 
 <script>
 import AddElementZone from '@baserow/modules/builder/components/elements/AddElementZone.vue'
 import containerElement from '@baserow/modules/builder/mixins/containerElement'
 import AddElementModal from '@baserow/modules/builder/components/elements/AddElementModal.vue'
+import ElementPreview from '@baserow/modules/builder/components/elements/ElementPreview.vue'
+import PageElement from '@baserow/modules/builder/components/page/PageElement.vue'
 
 export default {
   name: 'FormElement',
-  components: { AddElementModal, AddElementZone },
+  components: { PageElement, ElementPreview, AddElementModal, AddElementZone },
   mixins: [containerElement],
   methods: {
     showAddElementModal() {
