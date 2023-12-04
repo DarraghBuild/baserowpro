d4a0dddf0bb591f9678e7d912abd6fb727caa908
Jonathan Adeline <jonathan@baserow.io>
2023-12-04 19:03:44 +0400

    floating button

---
+++ web-frontend/modules/core/components/ButtonFloating.vue
@@ -0,0 +1,66 @@
+<template>
+  <button
+    class="button-floating"
+    :class="classes"
+    :disabled="disabled || loading"
+    v-on="$listeners"
+  >
+    <i v-if="!loading" class="button-floating__icon" :class="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      required: false,
+      type: String,
+      default: 'primary',
+      validator: function (value) {
+        return ['primary', 'secondary'].includes(value)
+      },
+    },
+    /**
+     * The icon that must be shown inside the button.
+     */
+    icon: {
+      required: true,
+      type: String,
+      default: '',
+    },
+    /**
+     * Wether the button is loading or not.
+     */
+    loading: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    position: {
+      required: false,
+      type: String,
+      default: 'relative',
+      validator: function (value) {
+        return ['relative', 'fixed'].includes(value)
+      },
+    },
+  },
+  computed: {
+    classes() {
+      const classObj = {
+        'button-floating--loading': this.loading,
+        'button-floating--fixed': this.position === 'fixed',
+        [`button-floating--${this.type}`]: this.type,
+        disabled: this.disabled,
+        active: this.active,
+      }
+      return classObj
+    },
+  },
+}
+</script>
