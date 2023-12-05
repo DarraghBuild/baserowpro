07eaa056262057667b4cd291602cf87f3bfb180d
Jonathan Adeline <jonathan@baserow.io>
2023-12-05 16:09:03 +0400

    continue

--- web-frontend/modules/core/components/Button.vue
+++ web-frontend/modules/core/components/Button.vue
@@ -3,91 +3,110 @@
     :is="tag === 'a' || href ? 'a' : 'button'"
     class="button"
     :class="classes"
-    :disable="disabled"
+    :disabled="disabled || loading"
     :active="active"
     v-bind.prop="customBind"
     v-on="$listeners"
   >
-    <i v-if="prependIcon" class="button__icon" :class="prependIcon" />
-    <slot></slot>
-    <i
-      v-if="appendIcon || icon"
-      class="button__icon"
-      :class="appendIcon ? appendIcon : icon"
-    />
+    <i v-if="icon" class="button__icon" :class="icon" />
+    <span class="button__label"><slot></slot></span>
   </component>
 </template>
 
 <script>
 export default {
   props: {
+    /**
+     * The HTML tag to use for the button. Available tags are: a, button.
+     */
     tag: {
-      // a - button
       required: false,
       type: String,
       default: 'button',
+      validator: function (value) {
+        return ['a', 'button'].includes(value)
+      },
     },
+    /**
+     * The size of the button.
+     */
     size: {
-      // tiny - normal - large
       required: false,
       type: String,
       default: '',
     },
+    /**
+     * The type of the button. Available types are: primary, secondary, danger.
+     */
     type: {
-      //  primary - success - warning - error - ghost - light - link
       required: false,
       type: String,
-      default: '',
+      default: 'primary',
+      validator: function (value) {
+        return ['primary', 'secondary', 'danger'].includes(value)
+      },
     },
-    prependIcon: {
-      required: false,
-      type: String,
-      default: '',
-    },
-    appendIcon: {
-      required: false,
-      type: String,
-      default: '',
-    },
+    /**
+     * The icon of the button.
+     */
     icon: {
       required: false,
       type: String,
       default: '',
     },
+    /**
+     * Wether the button is loading or not.
+     */
     loading: {
       required: false,
       type: Boolean,
       default: false,
     },
+    /**
+     * Wether the button is active or not.
+     */
     disabled: {
       required: false,
       type: Boolean,
       default: false,
     },
+    /**
+     * Make the button full width.
+     */
     fullWidth: {
       required: false,
       type: Boolean,
       default: false,
     },
-    active: {
-      required: false,
-      type: Boolean,
-      default: false,
-    },
-    overflow: {
-      required: false,
-      type: Boolean,
-      default: false,
-    },
+    /**
+     * If the button is a link, this is the href.
+     */
     href: {
       required: false,
       type: String,
-      default: '',
+      default: null,
     },
+    /**
+     * If the button is a link, this is the rel. Available values are: nofollow, noopener, noreferrer.
+     */
+    rel: {
+      required: false,
+      type: String,
+      default: null,
+      validator: function (value) {
+        return ['nofollow', 'noopener', 'noreferrer'].includes(value)
+      },
+    },
+    /**
+     * If the button is a link, this is the target. Available values are: _blank, _self, _parent, _top.
+     */
     target: {
       required: false,
       type: String,
-      default: 'self',
+      default: null,
+      validator: function (value) {
+        return ['_blank', '_self', '_parent', '_top'].includes(value)
+      },
     },
   },
   computed: {
@@ -100,7 +119,6 @@ export default {
         'button--full-width': this.fullWidth,
         'button--icon-only': hasIcon && !this.$slots.default,
         'button--loading': this.loading,
-        disabled: this.disabled,
         active: this.active && !this.loading && !this.disabled,
         'button--overflow': this.overflow,
       }
@@ -108,13 +126,17 @@ export default {
     },
     customBind() {
       const attr = {}
-      if (this.href) {
+      if (this.tag === 'a') {
         attr.href = this.href
+        attr.target = this.target
+        attr.rel = this.target
       }
-      if (this.target) {
-        attr.target = `_${this.target}`
-      }
-      return attr
+
+      return Object.keys(attr).forEach((key) => {
+        if (attr[key] === null) {
+          delete attr[key]
+        }
+      })
     },
   },
 }
